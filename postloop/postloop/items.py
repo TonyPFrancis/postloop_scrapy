3519f82ec3f874a0b781e4f381179bed46154222
TonyPFrancis <tony.612.4u@gmail.com>
2015-07-30 23:20:35 +0530

    adding items file

--- postloop/postloop/items.py
+++ postloop/postloop/items.py
@@ -5,10 +5,11 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Field, Item
 
 
-class PostloopItem(scrapy.Item):
+class PostloopItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = Field()
+    user_name = Field()
